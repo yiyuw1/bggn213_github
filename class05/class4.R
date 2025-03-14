78afed3f444cf537b4d40959624981d433ff46f0
Yiyu Wang <yyw002@ucsd.edu>
2025-03-14 14:08:15 -0700

    add gitignore and class files

---
+++ class05/class4.R
@@ -0,0 +1,11 @@
+#' ---
+#' title: "My first R Script report"
+#' author: "Yiyu Wang"
+#' date: "Jan. 22nd, 2025"
+#' ---
+
+#a
+plot(1:10)
+x <- 1:50
+plot(x,sin(x), typ="l", col="blue", lwd=3, xlab= "x label" )
+
